f51d7313fc5b565c548bc20ac381392ae48428b1
mancook <mancook@tddev003-u12.(none)>
2014-08-25 07:48:24 +0800

    On branch master  Changes to be committed:    (use "git reset HEAD <file>..." to unstage)
    	new file:   src/errmsg.c

---
+++ src/errmsg.c
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  errmsg.c
+ *
+ *    Description:  
+ *
+ *
+ *        Version:  1.0
+ *        Created:  08/25/2014 07:47:50 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Mr. Dennis Cao (dc), guilin.cao@teclub.com
+ *        Company:  Spring Tech, Shanghai
+ *
+ * =====================================================================================
+ */
+
+
+
+void main(){
+
+	return;
+}
