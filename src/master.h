43de2a74ffadc819aeff575ba4c9f5c267cead0e
Guilin Cao <guilin.cao@teclub.cn>
2014-08-25 09:20:09 +0800

    [src] master branch - add 'master.h';
    Changes to be committed:
    	new file:   src/master.h

---
+++ src/master.h
@@ -0,0 +1,31 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  master.h
+ *
+ *    Description:  
+ *
+ *
+ *        Version:  1.0
+ *        Created:  08/25/2014  09:18:48
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Mr. Dennis Cao (dc), guilin.cao@teclub.com
+ *        Company:  Spring Tech, Shanghai
+ *
+ * ---------------  History ------------------------------------------------------------
+ *
+ *     2014-08-25:  1.0 created in master branch;
+ *
+ * =====================================================================================
+ */
+
+#ifndef __MASTER_H__
+#define __MASTER_H__
+
+
+
+
+#endif // __MASTER_H__
+
